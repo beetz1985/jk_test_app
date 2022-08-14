e81c44d98a9d0efe99cee3f0058d532fb7fe6193
beetz1985 <benjamintoulson@hotmail.co.uk>
2022-08-14 23:51:03 +0100

    Used .Map to map over mock API data and inserted it into component. Reused component with additional mock API data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
  import Jokes from './Jokes'
- 
+ import jokesData from './jokesData'
+
+
  function App() {
+
+    const elementsJokes = jokesData.map(v=> <Jokes setup={v.setup} punch={v.punch} />);
+
     return (
         <article>
-            <Jokes 
-                setup="Lame Line" 
-                punch="Lame Joke"
-                />
-            <Jokes 
-                setup="what do you call..." 
-                punch="Lame Joke Again"
-                />
-            <Jokes 
-                setup="Knock knock" 
-                punch="Lame Joke who's there"
-                />
-            <Jokes 
-                setup="This is just a single puncline setup"
-                />
-            <Jokes 
-                setup={10}
-                />                
+           {elementsJokes}
         </article>
     )
  }
---
+++ src/jokesData.js
@@ -0,0 +1,24 @@
+
+
+const jokesData = [
+
+    {
+        setup: "Lame Line",
+        punch: "Lame Joke"
+    },
+    {
+        setup: "what do you call...",
+        punch: "Lame Joke Again"
+    },
+    {
+        setup: "Knock knock",
+        punch: "Lame Joke who's there"
+    },
+    {
+        setup: "Knock knock Knock",
+        punch: "Lame Joke who's there again - this is the last one"
+    }
+];
+
+
+export default jokesData
